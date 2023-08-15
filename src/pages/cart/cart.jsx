06ebb21d344750bf646b977f8d40496dd317795c
dev-from-cbd <veryfatwombat@gmail.com>
2023-08-16 06:18:59 +0930

    update

---
+++ src/pages/cart/cart.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const cart = () => {
+  return <div>cart</div>;
+};
---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const shop = () => {
+  return <div>shop</div>;
+};
