074edec62d1039032f5ce96f0881a23c6d31963e
dev-from-cbd <veryfatwombat@gmail.com>
2023-08-19 08:11:33 +0930

    update

--- src/products.jsx
+++ src/products.jsx
@@ -1,4 +1,8 @@
 import product1 from "./assets/1.png";
+import product2 from "./assets/2.png";
+import product3 from "./assets/3.png";
+import product4 from "./assets/4.png";
+import product5 from "./assets/5.png";
 
 export const PRODUCTS = [
   {
