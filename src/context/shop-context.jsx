33bb8518b8707cda3bcfe30d6e944aae585eb9fe
dev-from-cbd <veryfatwombat@gmail.com>
2023-08-23 06:43:09 +0930

    Update

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,7 +1,27 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const ShopContext = createContext(null);
 
-export const ShopContextProvider = () => {
-  return <div>Shop Context</div>;
+const getDefaultCart = () => {
+  let cart = {};
+  for (let i = 1; i < PRODUCTS.length; i++) {
+    cart[i] = 0;
+  }
+  return cart;
+};
+
+export const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemID) => {
+    setCartItems((prev) => ({ ...prev, [itemID]: prev[itemID] + 1 }));
+  };
+
+  const removeFromCart = (itemID) => {
+    setCartItems((prev) => ({ ...prev, [itemID]: prev[itemID] - 1 }));
+  };
+
+  const contextValue = { cartItems, addToCart, removeFromCart };
+
+  return <ShopContext.Provider>{props.children}</ShopContext.Provider>;
 };
