1818c349a969c4df28d5e7ff786d99363f49fedc
dev-from-cbd <veryfatwombat@gmail.com>
2023-08-28 07:36:28 +0930

    Update

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -3,12 +3,11 @@ import { ShopContext } from "../../context/shop-context";
 import { PRODUCTS } from "../../products";
 import { CartItem } from "./cart-item";
 import { useNavigate } from "react-router-dom";
-
 import "./cart.css";
+
 export const Cart = () => {
   const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
   const totalAmount = getTotalCartAmount();
-
   const navigate = useNavigate();
 
   return (
@@ -17,11 +16,11 @@ export const Cart = () => {
         <h1>Your Cart Items</h1>
       </div>
       <div className="cart">
-        {PRODUCTS.map((product) => {
-          if (cartItems[product.id] !== 0) {
-            return <CartItem data={product} />;
-          }
-        })}
+        {PRODUCTS.map((product) =>
+          cartItems[product.id] !== 0 ? (
+            <CartItem key={product.id} data={product} />
+          ) : null
+        )}
       </div>
 
       {totalAmount > 0 ? (
--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -4,15 +4,14 @@ import { ShopContext } from "../../context/shop-context";
 export const Product = (props) => {
   const { id, productName, price, productImage } = props.data;
   const { addToCart, cartItems } = useContext(ShopContext);
-
   const cartItemCount = cartItems[id];
 
   return (
     <div className="product">
-      <img src={productImage} />
+      <img src={productImage} alt={productImage} />
       <div className="description">
         <p>
-          <b>{productName}</b>
+          <strong>{productName}</strong>
         </p>
         <p> ${price}</p>
       </div>
